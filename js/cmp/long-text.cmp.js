d8c1d0bc97c04c326d320d5a9063a75999f0c6c8
Idov <magalidov@gmail.com>
2020-06-24 12:33:07 +0300

    more files

---
+++ js/cmp/long-text.cmp.js
@@ -0,0 +1,33 @@
+export default{
+    props: ['text'],
+    template: `
+    <section class="long-text flex col-layout">
+        <h3>Description:</h3>
+        <p>{{getFirstWords}}
+            <span v-show="readMore">{{getRestOfWords}}</span>
+            <button class="btn-more-less" @click="readMore=!readMore" v-show="isLong">{{getWord}}</button >
+        </p>
+    </section>
+    `,
+    data() {
+        return {
+            textToShow: this.text,
+            readMore: false,
+        }
+    },
+    computed: {
+        getFirstWords() {
+            return this.textToShow.substring(0, 100)
+        },
+        getRestOfWords() {
+            return this.textToShow.substring(100)
+        },
+        isLong(){
+            return this.text.length > 100
+        },
+        getWord(){
+            if(this.readMore)return 'Less..'
+            return 'More..'
+        }
+    },
+}
