cd60040d83336d72d5de381f0533362cde9d7810
Idov <magalidov@gmail.com>
2020-06-24 17:12:46 +0300

    email previews updated

--- js/apps/email/pages/email-app.cmp.js
+++ js/apps/email/pages/email-app.cmp.js
@@ -1,5 +1,3 @@
-import { emailService } from '../services/email-service.js';
-import emailList from '../cmps/email-list.cmp.js';
 // import emailFilter from '../cmps/email-filter.cmp.js';
 // import emailTags from '../cmps/email-tags.cmp.js';
 
@@ -8,22 +6,13 @@ export default {
     <section class="">
         <h1>Email</h1>
         <!-- <email-filter/> -->
-        <email-list v-if="emailsToShow" :emails="emailsToShow"/>
+        <router-view/>
         <!-- <email-tags/> -->
     </section>
     `,
-	data() {
-        return{
-            emailsToShow: null
-        }
-	},
-	created() {
-		emailService.loadEmails().then((loadedMails) => (this.emailsToShow = loadedMails));
-	},
 	computed: {},
 	methods: {},
 	components: {
-		emailList,
 		// emailFilter,
 		// emailTags,
 	},
