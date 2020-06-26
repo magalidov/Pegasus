1033b239d052dda59b07746374576506b9dd0fac
Asaf <casaf5@gmail.com>
2020-06-26 12:20:46 +0300

    homepage design changes

--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -1,16 +1,25 @@
 export default {
-    template:`
+  template: `
     <section class="app-home">
-
+            <h1>Welcome To Pegasus</h1>
+            <h3>Where Design and Productivity Combined</h3>
+            <div class="apps flex space-around wrap">
+                <section class="product col-layout">
+                    <h3 class="app-title">Books</h3>
+                    <i class="far fa-book fa-4x"></i></i>
+                    <p>Search,view and buy Your favorite books</p>
+                </section>
+                <section class="product col-layout">
+                    <h3 class="app-title">Email</h3>
+                    <i class="far fa-envelope fa-4x"></i></i>
+                    <p>Organize your mails in our brand new ,fresh looking email design</p>
+                </section>
+                <section class="product col-layout">
+                     <h3 class="app-title">Notes</h3>
+                     <i class="far fa-sticky-note fa-4x"></i>
+                     <p>Kepp track of the things your need to remember</p>
+                </section>
+            </div>
     </section>
     `,
-    computed:{
-
-    },
-    methods:{
-
-    },
-    components:{
-        
-    }
-}
+};
