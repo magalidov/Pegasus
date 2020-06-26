295644858017de1f15cb10a27395895edc857ad1
Asaf <casaf5@gmail.com>
2020-06-26 15:00:44 +0300

    flex layout !!! work please

--- js/pages/app-about.cmp.js
+++ js/pages/app-about.cmp.js
@@ -1,17 +1,20 @@
 export default {
-    template: `
+  template: `
     <div class="app-about">
-        <h1>About</h1>
-        <h1> -Idov And Asaf- </h1>
+       <h1>About Us</h1>
+        <section class="flex justify-center">
+        <section class="about-card">
+            <h2>Asaf</h2>
+            <h3>Fullstack Developer</h3>
+        </section>
+        <section  class="about-card">
+           <h2>Idov</h2>
+           <h3>Fullstack Developer</h3>
+        </section>
+        </section>
     </div>
     `,
-    computed: {
-
-    },
-    methods: {
-
-    },
-    components: {
-
-    }
-}
+  computed: {},
+  methods: {},
+  components: {},
+};
--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -4,17 +4,17 @@ export default {
             <h1>Welcome To Pegasus</h1>
             <h3>Where Design and Productivity Combined</h3>
             <div class="apps flex space-around wrap">
-                <section class="product col-layout">
+                <section class="product col-layout" @click="goto('books')">
                     <h3 class="app-title">Books</h3>
                     <i class="far fa-book fa-4x"></i></i>
                     <p>Search,view and buy Your favorite books</p>
                 </section>
-                <section class="product col-layout">
+                <section class="product col-layout" @click="goto('email')">
                     <h3 class="app-title">Email</h3>
                     <i class="far fa-envelope fa-4x"></i></i>
                     <p>Organize your mails in our brand new ,fresh looking email design</p>
                 </section>
-                <section class="product col-layout">
+                <section class="product col-layout" @click="goto('notes')">
                      <h3 class="app-title">Notes</h3>
                      <i class="far fa-sticky-note fa-4x"></i>
                      <p>Kepp track of the things your need to remember</p>
@@ -22,4 +22,10 @@ export default {
             </div>
     </section>
     `,
+    methods:{
+        goto(location){
+            this.$router.push(`/${location}`);
+        }
+    }
 };
+
